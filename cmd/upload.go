10909f76f57a8ad5380e5d6314ebba66842fc048
shilangyu <xmarcinmarcin@gmail.com>
2019-03-02 13:20:16 +0100

    printing out concerned files

---
+++ cmd/upload.go
@@ -0,0 +1,78 @@
+// Copyright © 2019 Marcin Wojnarowski xmarcinmarcin@gmail.com
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package cmd
+
+import (
+	"errors"
+	"fmt"
+	"log"
+	"os"
+	"path/filepath"
+
+	gitignore "github.com/monochromegane/go-gitignore"
+	"github.com/spf13/cobra"
+)
+
+// uploadCmd represents the upload command
+var uploadCmd = &cobra.Command{
+	Use:   "upload [output path]",
+	Short: "Upload files to your drive folder",
+	Long: `Uploads files from the current directory (can be overwritten with --input flag) to a drive folder
+It will ignore files that satisfy the .driveignore
+The order of importance of a .driveignore file:
+current folder > global config 
+`,
+	Run: func(cmd *cobra.Command, args []string) {
+		root, _ := os.Getwd()
+		driveignore, _ := gitignore.NewGitIgnore("./.driveignore")
+		err := filepath.Walk(root, func(path string, info os.FileInfo, err error) error {
+			path, _ = filepath.Rel(root, path)
+			if err != nil {
+				log.Fatal(err)
+			}
+			if info.IsDir() && driveignore.Match(path, true) {
+				return filepath.SkipDir
+			}
+			if driveignore.Match(path, false) {
+				return nil
+			}
+			fmt.Println(path)
+			return nil
+		})
+		if err != nil {
+			panic(err)
+		}
+	},
+	Args: func(cmd *cobra.Command, args []string) error {
+		if len(args) != 1 {
+			return errors.New("requires only one argument")
+		}
+		if _, err := os.Stat(args[0]); os.IsNotExist(err) {
+			return errors.New("passed path is invalid")
+		}
+		return nil
+	},
+}
+
+var input string
+var mergeIgnores bool
+
+func init() {
+	rootCmd.AddCommand(uploadCmd)
+
+	// Local flags
+	uploadCmd.Flags().StringVarP(&input, "input", "i", "./", "Input directory of the files to be uploaded")
+	uploadCmd.Flags().BoolVarP(&mergeIgnores, "mergeIgnores", "M", false, "Merges global and current dir .driveignore")
+}
